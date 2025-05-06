6ba5900b61b802b3ce3d8984eb22d66ac8348354
DeepJoshi07 <143418039+DeepJoshi07@users.noreply.github.com>
2025-05-06 17:40:04 +0530

    Update AuthContext.jsx

--- LinkedIn/frontend/src/Context/AuthContext.jsx
+++ LinkedIn/frontend/src/Context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react'
 export const authDataContext = createContext()
 
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:4000"
+    let serverUrl = "https://linkedin-backend-5tew.onrender.com"
     let value ={
         serverUrl,
     }
